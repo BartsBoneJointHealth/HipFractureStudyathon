216286b0e8e4750cedbe4a3e776705980270d43e
ZorzArg <george.argyriou@odysseusinc.com>
2023-09-25 11:12:11 +0100

    added creds

---
+++ creds.R
@@ -0,0 +1,9 @@
+
+
+## Username            = OmopAtlas
+## Password            = kiJu765dgC!gftR
+## String              = jdbc:sqlserver://SH-DC4-LOMOP02;database=OMOP;integratedSecurity=false;user=OmopAtlas;password=kiJu765dgC!gftR;
+## CDM Schema Name     = dbo
+## Vocab Schema Name   = dbo
+## Work schema name    = results
+
